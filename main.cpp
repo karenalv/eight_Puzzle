3dcc13f5fd7fce2f42b81c2f840339f925fa47eb
karenalv <131569376+karenalv@users.noreply.github.com>
2025-02-06 11:06:14 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// struct Node{
+//     vector<vector<int>> state;
+//     int cost;
+//     int estimate;
+    
+// }
+
+int main(){
+    int puzzleChoice=0;
+    cout<< "8-puzzle program:"<< endl;
+    cout<< "Would you like to input your own puzzle(1) or recieve a random puzzle(2)"<<endl;
+    cin>> puzzleChoice;
+    cout<< puzzleChoice;
+
+    return 0; 
+}
