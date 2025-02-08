acb8ed237ef83d434b886f1bc07cfc1beec4d82b
karen alvarez <karenalvarez@Mac.attlocal.net>
2025-02-07 22:53:28 -0800

    added duplicate function+ deleted commented out code

--- main.cpp
+++ main.cpp
@@ -11,16 +11,15 @@ struct Board{
     int blankX; //x and y pos for blank spot 
     int blankY;
     shared_ptr<Board> original; //original board
-
     //strufct for board only, the G, H and F are found in later funct
     Board(vector<vector<int> > b, int x, int y,shared_ptr<Board> p = nullptr)
         :board(b), ucs(0), heur(0),totCost(0), blankX(x), blankY(y), original(p) {}
-
     // struct for full board, when prog basically done
     Board(vector<vector<int> > b, int g, int h, int x, int y, shared_ptr<Board> p = nullptr )
     :board(b), ucs(g), heur(h), totCost(g + h), blankX(x), blankY(y), original(p){}
     
 };
+
 
 vector<Board> randomBoard(){
     vector <Board> rands; //boards , we know depth of (test cases)
@@ -36,36 +35,41 @@ vector<Board> randomBoard(){
     return rands;
 }
 
+
+void checkDuplicates(const vector<int>&boardNums){
+    for(int i =0; i<boardNums.size(); i++){
+        for(int j=i+1; i<boardNums.size();j++){
+            if(boardNums[i]== boardNums[j]){
+                cout<< "Please use each number only once."<<endl;
+                exit(0);
+            }
+        }
+    }
+}
+
+
 int puzzleRun(int choice){
     vector<vector<int> > board(3, vector<int>(3)); //initalize board for opt 1 or 2
     int blankX = -1;
     int blankY = -1;
+    vector<int> boardNums; //array w numbers
     Board boardStart({{}}, -1, -1); //declaring so no issues declaring inside the if statments
     if(choice ==1){
         //take in array/ struct
-        cout<< "Enter 9 integers for the board. Enter 0 for the blank space"<<endl;
+        cout<< "Enter 9 integers for the board. Enter 0 for the blank space."<<endl;
         for(int i=0; i<3; i++){
             for(int j=0; j <3; j++){
                 cin>>board[i][j];
+                boardNums.push_back(board[i][j]);
                 if(board[i][j] ==0){
                     blankX = i;
                     blankY = j;
                 }
             }
         }
+        checkDuplicates(boardNums);
         boardStart = Board(board, blankX, blankY);
-        // Board b(board, 2,2,2,2, nullptr);
-        // //print board
-        // cout << "\nBoard configuration:\n";
-        // for (const auto &row : boardStart.board) {
-        //     for (int num : row) {
-        //         cout << num << " ";
-        //     }
-        //     cout << endl;
-        // }
-
-    }
-    else if(choice ==2){
+    } else if(choice ==2){
         //rand function
         vector<Board> boards = randomBoard();
         //cout << "Number of predefined boards: " << boards.size() << endl;
@@ -73,27 +77,17 @@ int puzzleRun(int choice){
         int randB=(rand() % boards.size()); //finds rand number and chooses that board to be used
         //cout << "Selected random board index: " << randB << endl; //makes sure boards are there and bening passed in
         boardStart = boards[randB]; // now it reassigns insteaed of redeclaring which is why wasnt printifng
-
     }
-
-    // cout << "\nBoard configuration:\n";
-    //     for (const auto &row : boardStart.board) {
-    //         for (int num : row) {
-    //             cout << num << " ";
-    //         }
-    //         cout << endl;
-    //     }
 
     return 0;
 }
 
 
-
 int main(){
     int puzzleChoice=0;
     cout<< "8-puzzle program:"<< endl;
-    cout<< "Would you like to: input your own puzzle(1), recieve a random puzzle(2), quit (3)"<<endl;
-    cout<< "Input one number at a time followed by the 'enter' key"<< endl;
+    cout<< "Would you like to: input your own puzzle(1), recieve a random puzzle(2), quit(3)"<<endl;
+    cout<< "Input one number at a time followed by the 'enter' key."<< endl;
     cin>> puzzleChoice;
     if(puzzleChoice == 1){
         //input puzzle
@@ -104,14 +98,9 @@ int main(){
     } else if(puzzleChoice == 3){
         return 0;
     } else{
-        cout<< "Invalid input"<< endl;
+        cout<< "Invalid input."<< endl;
         return 0;
     }
-
-    //int final = puzzleRun(puzzleChoice);
-    //cout<< puzzleChoice;
-
-    
 
     return 0; 
 }
